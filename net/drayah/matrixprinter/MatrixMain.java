93ee92d7e807d491a5a57cf9e370a55e271aaa8e
drayah <drayah@c1691102-4536-0410-980b-a127417845c5>
2007-07-28 14:51:35 +0000

    fixed comments on MatrixMain.java

--- net/drayah/matrixprinter/MatrixMain.java
+++ net/drayah/matrixprinter/MatrixMain.java
@@ -43,13 +43,13 @@ public class MatrixMain {
         if (escp.initialize()) {
             escp.setCharacterSet(ESCPrinter.USA);
             escp.select15CPI(); //15 characters per inch printing
-            escp.advanceVertical(5); //go down 15cm
-            escp.setAbsoluteHorizontalPosition(5); //15cm to the right
+            escp.advanceVertical(5); //go down 5cm
+            escp.setAbsoluteHorizontalPosition(5); //5cm to the right
             escp.bold(true);
             escp.print("Let's print some matrix text ;)");
             escp.bold(false);
             escp.advanceVertical(1);
-            escp.setAbsoluteHorizontalPosition(5); //back to 15cm on horizontal axis
+            escp.setAbsoluteHorizontalPosition(5); //back to 5cm on horizontal axis
             escp.print("Very simple and easy!");            
             escp.formFeed(); //eject paper
             escp.close(); //close stream
